a1dbaf52c77f50f73c5434d9ccd5ce24e02cce74
Basit Raza <geek96@outlook.com>
2018-03-17 01:35:05 +0500

    Add each address tx hash in api response!

--- wallet/views/transfer.py
+++ wallet/views/transfer.py
@@ -1,10 +1,15 @@
 from rest_framework import views
 from rest_framework.response import Response
-
+from utils.web3 import Web3Util
 
 class TransferView(views.APIView):
 
     def post(self, request, format=None):
+        outs = []
         for obj in request.data:
             print("Transferring [{}]ETH to wallet [{}]".format(obj['amount'], obj['address']))
-        return Response({'data': 'transfer request submitted!'})
+            txid = Web3Util().send_transaction(obj['address'], obj['amount'])
+            out = obj.copy()
+            out.update({'tx_hash': txid})
+            outs.append(out)
+        return Response({'data': outs})
